7d698eb04cd831377519166eaab52e50cdd9b4a2
Martin Lind <martin@spegelreflex.se>
2021-05-14 11:48:53 +0200

    Pie charts in part 3

---
+++ mplt_3_popular_languages_pie.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+
+plt.style.use('fivethirtyeight')
+
+slices = [59219, 55466, 47544, 36443, 35917]
+labels = ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java']
+explode = [0, 0, 0, 0.1, 0]
+
+plt.pie(slices, labels=labels, explode=explode, shadow=True,
+        startangle=90, autopct='%1.1f%%',
+        wedgeprops={'edgecolor': 'black'})
+
+plt.title('Top 5 mosts popular languages')
+plt.tight_layout()
+plt.show()
